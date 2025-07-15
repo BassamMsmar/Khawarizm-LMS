8a16fef0b9e195bcff1c9b3868ec0290c1c8d958
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-07-15 19:20:39 +0300

    make lessons details

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Course
+from .models import Course, Lesson
 from django.views.generic import ListView, DetailView
 
 # Create your views here.
@@ -12,7 +12,45 @@ class CourseList(ListView):
     context_object_name = 'courses'
 
 
-class CourseDetail(DetailView):
+# class CourseDetail(DetailView):
+#     model = Course
+#     template_name = 'course_detail.html'
+#     context_object_name = 'course'
+
+
+from django.views.generic import DetailView
+from django.shortcuts import render
+from django.http import Http404
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Course, Unit
+
+class CourseDetail(LoginRequiredMixin, DetailView):
     model = Course
     template_name = 'course_detail.html'
     context_object_name = 'course'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        course = self.get_object()
+
+        # Get units related to the course
+        units = Unit.objects.filter(course=course)
+
+        # Get lessons for each unit
+        unit_lessons = {
+            unit: unit.lessons.all() for unit in units
+        }
+
+        context["related_units"] = units
+        context["unit_lessons"] = unit_lessons
+
+        return context
+
+    
+
+class Lesson_Detail(DetailView):
+    model = Lesson
+    template_name = 'lesson.html'
+    context_object_name = 'lesson'
+    slug_url_kwarg = 'lesson_slug'
