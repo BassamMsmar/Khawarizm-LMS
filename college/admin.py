7ac82b550d7b17ec308eebb56a0261f114a2e0e2
bassammsmar <bassammsmar@gmail.com>
2025-07-17 13:50:04 +0300

    fix : cercale in models betewen department and dager

--- college/admin.py
+++ college/admin.py
@@ -1,11 +1,4 @@
 from django.contrib import admin
 from .models import College, Question
 
-class CollegeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'max_students', 'is_public', 'regular_price', 'discounted_price', 'tags', 'questions')
-    list_filter = ('title', 'max_students', 'is_public', 'regular_price', 'discounted_price', 'tags', 'questions')
-    search_fields = ('title', 'max_students', 'is_public', 'regular_price', 'discounted_price', 'tags', 'questions')
-    ordering = ('title', 'max_students', 'is_public', 'regular_price', 'discounted_price', 'tags', 'questions')
 
-admin.site.register(College, CollegeAdmin)
-admin.site.register(Question)
--- degreeLevel/admin.py
+++ degreeLevel/admin.py
@@ -1,12 +1,4 @@
 from django.contrib import admin
-from .models import DegreeLevel
 
-class DegreeLevelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'college', 'department', 'courses',  'is_active', )
-    list_filter = ('name', 'user', 'college', 'department', 'courses', 'is_active', )
-    search_fields = ('name', 'user', 'college', 'department', 'courses', 'is_active', )
-    ordering = ('name', 'user', 'college', 'department', 'courses', 'is_active', )
-
-admin.site.register(DegreeLevel, DegreeLevelAdmin)
 
 # Register your models here.
