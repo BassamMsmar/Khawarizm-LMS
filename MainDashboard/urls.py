872f808e8d97da3d753ccfde917bd41f2c2d998f
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-08-04 10:12:29 +0300

    ADD: full fronend for dashboard

--- MainDashboard/urls.py
+++ MainDashboard/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('dashboard', views.dashboard)
+    path('dashboard', views.dashboard),
+    path('courses', views.courses),
+    path('lessons', views.lessons),
+    path('students', views.students),
+    path('quizzes', views.quizzes),
+    path('reports', views.reports),
+    path('settings', views.settings),
   
 ]
--- MainDashboard/views.py
+++ MainDashboard/views.py
@@ -3,4 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'pages/index.html')
+    return render(request, 'pages/dashboard.html')
+
+
+
+def courses(request):
+    return render(request, 'pages/courses.html')
+
+
+def lessons(request):
+    return render(request, 'pages/lessons.html')
+
+
+def quizzes(request):
+    return render(request, 'pages/quizzes.html')
+
+def students(request):
+    return render(request, 'pages/students.html')
+
+
+def reports(request):
+    return render(request, 'pages/reports.html')
+
+
+def settings(request):
+    return render(request, 'pages/settings.html')
