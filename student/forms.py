bac9485a2492577864cb60a2de92d7d7e3b5c1c8
Mohamed00Abdelmonem <mmohamedabdelm@gmail.com>
2025-09-07 10:16:33 +0300

    update my-payment

--- student/forms.py
+++ student/forms.py
@@ -4,4 +4,5 @@ from .models import Payment
 class PaymentForm(forms.ModelForm):
     class Meta:
         model = Payment
-        fields = ['amount_paid']
+        fields = ['amount_paid', 'reference_number', 'receipt_image']  # الحقول اللي الطالب يدخلها
+
--- student/models.py
+++ student/models.py
@@ -9,10 +9,19 @@ class Student(models.Model):
     def __str__(self):
         return self.user.email
 
+
+
+
 class Payment(models.Model):
-    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    student = models.ForeignKey("Student", on_delete=models.CASCADE)
     amount_paid = models.DecimalField(max_digits=10, decimal_places=2)
     payment_date = models.DateField(auto_now_add=True)
 
+    # الرقم المرجعي للعملية
+    reference_number = models.CharField(max_length=100)
+
+    # صورة الايصال
+    receipt_image = models.ImageField(upload_to='receipts/', blank=True, null=True)
+
     def __str__(self):
-        return f"Payment of {self.amount_paid} for {self.student.user.username}"
+        return f"Payment {self.reference_number} of {self.amount_paid} for {self.student.user.username}"
