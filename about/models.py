941bccc6e15cf88b54d2f13d20c1146d4cb5cf9e
Bassam Msmar <48624392+BassamMsmar@users.noreply.github.com>
2025-12-24 13:48:41 +0300

    edit about app to add title and descriptions

--- about/models.py
+++ about/models.py
@@ -4,11 +4,15 @@ from django.db import models
 
 
 class AboutPage(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    is_active = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name="Title", null=True, blank=True)
+    sub_title = models.CharField(max_length=200, verbose_name="Sub Title", null=True, blank=True)
+    image = models.ImageField(upload_to='about/images', verbose_name="Image", null=True, blank=True)
+    video = models.FileField(upload_to='about/videos', verbose_name="Video", null=True, blank=True)
+    description = models.TextField(verbose_name="Description", null=True, blank=True)
+    content = models.TextField(verbose_name="Content", null=True, blank=True)
+    is_active = models.BooleanField(default=True, verbose_name="Is Active")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
 
     def __str__(self):
         return self.title
